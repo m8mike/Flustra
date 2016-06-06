605894abebece8e3045acdcb906c4dabfc3f8051
m8mike <m8m8ke@gmail.com>
2016-06-06 17:56:54 +0300

    update

--- logic.js
+++ logic.js
@@ -8,13 +8,59 @@ var sketchProc = function(processingInstance) {
 			lp = new LayersPanel(window.innerWidth - 210, 10, 200, window.innerHeight - 20);
 			lp.draw();
 		};
-		var LayersPanel = function(x, y, w, h) {
+		var ToolButton = function(x, y, name, action) {
 			this.x = x;
 			this.y = y;
-			this.w = w;
-			this.h = h;
+			this.w = 14;
+			this.h = 14;
+			this.name = name;
+			this.action = action;
+			this.pressed = false;
+			this.mouseIsOver = false;
+		};
+		ToolButton.prototype.draw = function() {
+			if (this.pressed && this.mouseIsOver) {
+				stroke(0, 0, 0);
+				fill(41, 41, 41);
+			} else if (this.mouseIsOver) {
+				stroke(255, 255, 255);
+				fill(145, 145, 145);
+			} else {
+				noStroke();
+				fill(77, 77, 77);
+			}
+			rect(this.x, this.y, this.w, this.h);
+		};
+		ToolButton.prototype.onOver = function() {
+			if (mouseX > this.x && mouseX < this.x + 14 &&
+				mouseY > this.y && mouseY < this.y + 14) {
+				this.mouseIsOver = true;
+			} else {
+				this.mouseIsOver = false;
+			}
+			return this.mouseIsOver;
+		};
+		ToolButton.prototype.onPressed = function() {
+			this.pressed = this.onOver();
+		};
+		ToolButton.prototype.onReleased = function() {
+			this.pressed = this.onOver();
+			if (this.pressed && this.action) {
+				this.action();
+			}
+			this.pressed = false;
+		};
+		var trace = function() {
+			println("hello");
+		};
+		var LayersPanel = function(x, y) {
+			this.x = x;
+			this.y = y;
+			this.w = 100;
+			this.h = 300;
 			this.movingStarted = false;
 			this.sideMoving = '';
+			this.newLayerButton = new ToolButton(this.x + 55, this.y + this.h - 14, "New Layer", trace);
 		};
 		LayersPanel.prototype = Object.create(ResizablePanel.prototype);
 		LayersPanel.prototype.draw = function() {
@@ -22,6 +68,18 @@ var sketchProc = function(processingInstance) {
 			fill(255, 255, 255);
 			text("Layers", this.x + 5, this.y + 12);
 			text("0 Layers", this.x + 5, this.y + this.h - 3);
+			this.newLayerButton.draw();
+		};
+		LayersPanel.prototype.resize = function() {
+			ResizablePanel.prototype.resize.call(this);//call superclass method
+			this.newLayerButton.x = this.x + 55;
+			this.newLayerButton.y = this.y + this.h - 14;
+		};
+		LayersPanel.prototype.onPressed = function() {
+			this.newLayerButton.onPressed();
+		};
+		LayersPanel.prototype.onReleased = function() {
+			this.newLayerButton.onReleased();
 		};
 		var Layer = function(name) {
 			this.name = name;
@@ -29,19 +87,8 @@ var sketchProc = function(processingInstance) {
 			this.locked = false;
 			this.color = color(random(0, 255), random(0, 255), random(0, 255));
 		};
-		var ToolButton = function(x, y, w, h, name, action) {
-			this.x = x;
-			this.y = y;
-			this.w = w;
-			this.h = h;
-			this.name = name;
-			this.action = action;
-		};
-		ToolButton.prototype.draw = function() {
-			stroke(0, 0, 0);
-			fill(77, 77, 77);
-			rect(this.x, this.y, this.w, this.h);
-		};
+		var lp = new LayersPanel(290, 10);
+		lp.draw();
 		draw = function() {
 			background(255, 255, 255);
 			lp.draw();
@@ -54,10 +101,15 @@ var sketchProc = function(processingInstance) {
 				lp.checkSide();
 				if (lp.sideMoving !== '') {
 					lp.movingStarted = true;
+				} else {
+					lp.onPressed();
 				}
 			}
 		};
 		mouseReleased = function() {
+			if (mouseButton === LEFT) {
+				lp.onReleased();
+			}
 			if (lp.movingStarted && mouseButton === LEFT) {
 				lp.movingStarted = false;
 				lp.sideMoving = '';
