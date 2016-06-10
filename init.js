29aabd18a45b039244a2a6df67a98b35a1ef23d0
m8mike <m8m8ke@gmail.com>
2016-06-10 21:32:30 +0300

    java syntax

--- init.js
+++ init.js
@@ -1,46 +1,40 @@
 var lp;
-var sketchProc = function(processingInstance) {
-	with (processingInstance) {
-		setup = function() {
-			size(window.innerWidth, window.innerHeight);
-			//background(255, 255, 255);
-			lp = new LayersPanel(window.innerWidth - 210, 10, 210, window.innerHeight - 20);
-			lp.layers.push(new Layer("Layer 1", null));
-			lp.draw();
-			/*canvas.addEventListener('mousewheel',function(event){
-				mouseController.wheel(event);
-				return false; 
-			}, false);*/
-		};
-		draw = function() {
-			background(255, 255, 255);
-			lp.draw();
-			if (lp.movingStarted) {
-				lp.resize();
-			}
-		};
-		void mousePressed() {
-			console.log("pressed");
-			if (!lp.movingStarted && mouseButton === LEFT) {
-				lp.checkSide();
-				if (lp.sideMoving !== '') {
-					lp.movingStarted = true;
-				} else {
-					lp.onPressed();
-				}
-			}
-		};
-		mouseReleased = function() {
-			console.log("released");
-			if (mouseButton === LEFT) {
-				lp.onReleased();
-			}
-			if (lp.movingStarted && mouseButton === LEFT) {
-				lp.movingStarted = false;
-				lp.sideMoving = '';
-			}
-		};
+void setup() {
+	size(window.innerWidth, window.innerHeight);
+	//background(255, 255, 255);
+	lp = new LayersPanel(window.innerWidth - 210, 10, 210, window.innerHeight - 20);
+	lp.layers.push(new Layer("Layer 1", null));
+	lp.draw();
+	/*canvas.addEventListener('mousewheel',function(event){
+		mouseController.wheel(event);
+		return false; 
+	}, false);*/
+}
+var draw() {
+	background(255, 255, 255);
+	lp.draw();
+	if (lp.movingStarted) {
+		lp.resize();
 	}
-};
-var canvas = document.getElementById("canvas");
-var processingInstance = new Processing(canvas, sketchProc); 
+}
+void mousePressed() {
+	console.log("pressed");
+	if (!lp.movingStarted && mouseButton === LEFT) {
+		lp.checkSide();
+		if (lp.sideMoving !== '') {
+			lp.movingStarted = true;
+		} else {
+			lp.onPressed();
+		}
+	}
+}
+void mouseReleased() {
+	console.log("released");
+	if (mouseButton === LEFT) {
+		lp.onReleased();
+	}
+	if (lp.movingStarted && mouseButton === LEFT) {
+		lp.movingStarted = false;
+		lp.sideMoving = '';
+	}
+}
