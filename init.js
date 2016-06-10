defc77d3bb82b0e103720fc1ca29d3c5bd3b97df
m8mike <m8m8ke@gmail.com>
2016-06-10 20:58:44 +0300

    script to init

--- init.js
+++ init.js
@@ -1,2 +1,42 @@
+var lp;
+var sketchProc = function(processingInstance) {
+	with (processingInstance) {
+		setup = function() {
+			size(window.innerWidth, window.innerHeight);
+			//background(255, 255, 255);
+			lp = new LayersPanel(window.innerWidth - 210, 10, 210, window.innerHeight - 20);
+			lp.layers.push(new Layer("Layer 1", null));
+			lp.draw();
+			/*canvas.addEventListener('mousewheel',function(event){
+				mouseController.wheel(event);
+				return false; 
+			}, false);*/
+		};
+		draw = function() {
+			background(255, 255, 255);
+			lp.draw();
+			if (lp.movingStarted) {
+				lp.resize();
+			}
+			if (!lp.movingStarted && mousePressed && mouseButton === LEFT) {
+				lp.checkSide();
+				if (lp.sideMoving !== '') {
+					lp.movingStarted = true;
+				} else {
+					lp.onPressed();
+				}
+			}
+		};
+		mouseReleased = function() {
+			if (mouseButton === LEFT) {
+				lp.onReleased();
+			}
+			if (lp.movingStarted && mouseButton === LEFT) {
+				lp.movingStarted = false;
+				lp.sideMoving = '';
+			}
+		};
+	}
+};
 var canvas = document.getElementById("canvas");
 var processingInstance = new Processing(canvas, sketchProc); 
