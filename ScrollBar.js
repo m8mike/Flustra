231f339992f79d37a66aec103865522cdbc3aabd
m8mike <m8m8ke@gmail.com>
2016-06-11 16:12:20 +0300

    3d scrollbar

--- ScrollBar.js
+++ ScrollBar.js
@@ -15,10 +15,16 @@ ScrollBar.prototype.resize = function(x, y, h) {
     this.size = this.maxSize;
 };
 ScrollBar.prototype.draw = function() {
+	stroke(0, 0, 0);
     fill(0, 0, 0);
     rect(this.x, this.y, this.w, this.w);
     fill(0, 0, 0);
     rect(this.x, this.y + this.h - this.w, this.w, this.w);
-    fill(255, 0, 0);
+	stroke(255, 0, 0);
+    fill(0, 0, 0);
+    rect(this.x-1, this.y + this.w + this.pos*(this.h - 2*this.w), this.w, this.size);
+	stroke(0, 255, 255);
+    rect(this.x+1, this.y + this.w + this.pos*(this.h - 2*this.w), this.w, this.size);
+	stroke(0, 0, 0);
     rect(this.x, this.y + this.w + this.pos*(this.h - 2*this.w), this.w, this.size);
 };
